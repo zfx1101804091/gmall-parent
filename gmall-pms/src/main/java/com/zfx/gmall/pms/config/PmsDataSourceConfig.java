96e8e0490ced939c62d532c63bb8235ff2bc493a
zfx1101804091 <1875586562@qq.com>
2020-01-31 23:16:44 +0800

    多数据源shardingjdbc读写分离测试

---
+++ gmall-pms/src/main/java/com/zfx/gmall/pms/config/PmsDataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.zfx.gmall.pms.config;
+
+import io.shardingjdbc.core.api.MasterSlaveDataSourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Configuration
+public class PmsDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() throws IOException, SQLException {
+        //使用sharding-jdbc创建出具有主从库的数据源
+        File file = ResourceUtils.getFile("classpath:sharding-jdbc.yml");
+        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(file);
+        return  dataSource;
+    }
+}
--- gmall-pms/src/test/java/com/zfx/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/zfx/gmall/pms/GmallPmsApplicationTests.java
@@ -1,10 +1,15 @@
 package com.zfx.gmall.pms;
 
+import com.baomidou.mybatisplus.extension.service.additional.update.impl.UpdateChainWrapper;
+import com.zfx.gmall.pms.entity.Brand;
 import com.zfx.gmall.pms.entity.Product;
+import com.zfx.gmall.pms.service.BrandService;
 import com.zfx.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class GmallPmsApplicationTests {
@@ -13,10 +18,46 @@ public class GmallPmsApplicationTests {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     public void contextLoads() {
         Product byId = productService.getById(1);
-        System.out.println(byId);
+        System.out.println(byId.getName());
     }
 
+    /*
+    * 测试增删改在主库，查询在从库
+    * */
+    @Test
+    public void testC() {
+
+        Brand brand = new Brand();
+        brand.setName("大宝剑").setFirstLetter("D").setProductCommentCount(100);
+        boolean save = brandService.save(brand);
+        System.out.println("save----"+save);
+    }
+
+    @Test
+    public void testU() {
+
+        Brand brand = new Brand();
+        brand.setName("大宝剑").setFirstLetter("D").setProductCommentCount(100).setBrandStory("爱的故事").setId(53L);
+        boolean update = brandService.updateById(brand);
+        System.out.println("update----"+update);
+    }
+
+    @Test
+    public void testD() {
+        boolean remove = brandService.removeById(53L);
+        System.out.println("remove----"+remove);
+    }
+
+
+    @Test
+    public void testR() {
+        Brand byId = brandService.getById(54L);
+        System.out.println("查询的数据----"+byId.getName());
+    }
 }
