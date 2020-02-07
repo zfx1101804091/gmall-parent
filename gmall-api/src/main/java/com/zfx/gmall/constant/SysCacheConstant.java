5a12be312f547d8a8990e49d6f9622519855251c
zfx1101804091 <1875586562@qq.com>
2020-02-08 00:04:58 +0800

    菜单数据缓存改造

---
+++ gmall-api/src/main/java/com/zfx/gmall/constant/SysCacheConstant.java
@@ -0,0 +1,10 @@
+package com.zfx.gmall.constant;
+
+/**
+ *
+ *  Redis的缓存全局变量
+ */
+public class SysCacheConstant {
+    //系统菜单
+    public static final String CATEGORY_MENU_CACHE_KEY= "sys_ category";
+}
--- gmall-pms/src/main/java/com/zfx/gmall/pms/service/impl/ProductCategoryServiceImpl.java
+++ gmall-pms/src/main/java/com/zfx/gmall/pms/service/impl/ProductCategoryServiceImpl.java
@@ -2,11 +2,14 @@ package com.zfx.gmall.pms.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zfx.gmall.constant.SysCacheConstant;
 import com.zfx.gmall.pms.entity.ProductCategory;
 import com.zfx.gmall.pms.mapper.ProductCategoryMapper;
 import com.zfx.gmall.pms.service.ProductCategoryService;
 import com.zfx.gmall.vo.product.PmsProductCategoryWithChildrenItem;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,6 +22,7 @@ import java.util.List;
  * @author zheng_fx
  * @since 2020-01-31
  */
+@Slf4j
 @Service
 @Component
 public class ProductCategoryServiceImpl extends ServiceImpl<ProductCategoryMapper, ProductCategory> implements ProductCategoryService {
@@ -26,8 +30,23 @@ public class ProductCategoryServiceImpl extends ServiceImpl<ProductCategoryMappe
     @Autowired
     ProductCategoryMapper productCategoryMapper;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Override
     public List<PmsProductCategoryWithChildrenItem> listProductCategoryChildren(Integer i) {
-        return productCategoryMapper.listProductCategoryChildren(i);
+        //改造菜单数据存redis
+        List<PmsProductCategoryWithChildrenItem> menuItems =null;
+        Object cacheMenu = redisTemplate.opsForValue().get(SysCacheConstant.CATEGORY_MENU_CACHE_KEY);
+        if(cacheMenu!=null){
+            //说明redis存在这个key，直接从redis中取,强转下
+            log.debug("######本次使用菜单缓存########");
+            menuItems= (List<PmsProductCategoryWithChildrenItem>) cacheMenu;
+        }else {
+            //缓存中没有，直接从数据库查，再放入缓存
+            menuItems= productCategoryMapper.listProductCategoryChildren(i);
+            redisTemplate.opsForValue().set(SysCacheConstant.CATEGORY_MENU_CACHE_KEY,menuItems);
+        }
+        return menuItems;
     }
 }
