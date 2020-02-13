461f1d8fa6b2b904bd7b00f894fb254f994af6c1
zfx1101804091 <1875586562@qq.com>
2020-02-13 22:53:11 +0800

    ①对象.方法（）才可以调用事务②本类调用自己的方法，相当于用的同一个事务saveProduct，所以此种方式是用不到事务的，使用代理对象.方法（）可以使用到事务

--- gmall-pms/src/main/java/com/zfx/gmall/pms/service/impl/ProductServiceImpl.java
+++ gmall-pms/src/main/java/com/zfx/gmall/pms/service/impl/ProductServiceImpl.java
@@ -15,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
@@ -46,6 +48,9 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
     @Autowired
     SkuStockMapper skuStockMapper;
 
+
+    //spring的所有组件全是单例，一定会出现线程安全问题
+    //只要没有共享属性，一个要读，一个要改，就不会出现安全问题；
     //当前线程共享同样的数据 把下面需要使用的productId 存入
     private static final ThreadLocal<Long> threadLocal =  new ThreadLocal();
     //ThreadLocal底层原理
@@ -84,22 +89,82 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
         return vo;
     }
 
+    /**
+     * 事务的传播行为：
+     * Propagation {
+     *     【REQUIRED】--此方法需要事务，如果没有就开新事务，如果之前已存在就用以前事务
+     *     SUPPORTS--支持：有事务用事务，没有不用
+     *     MANDATORY--强制要求： 必须在事务中运行，没有就报错
+     *     【REQUIRES_NEW】--需要新的：这个方法必须用一个新的事务来做，不用混用
+     *     NOT_SUPPORTED--不支持：此方法不能在事务中运行，如果有事务，暂停之前的事务；
+     *     NEVER--从不用事务，否则抛异常
+     *     NESTED--内嵌事务；还原点
+     *
+     *
+     *  REQUIRED【和大方法用一个事务】
+     *  REQUIRES_NEW【用一个新事务】
+     *  异常机制还是异常机制
+     * #####################################
+     * Required_ new
+     * 	外事务{
+     * 	A () ; Required; A
+     * 	B () ;Requires_new B
+     * 	try{
+     * 		C();Required; C
+     *        }catch(Exception e){
+     * 		//c出异常?
+     *    }
+     * 	D();Requires_new; D
+     *
+     * 	//给数据库存--外
+     * 	// int i = 10/0;
+     *
+     * 场景1:
+     * 	A方法出现了异常;由于异常机制导致代码停止，下面无法执行，数据库什么都没有
+     * 场景2:
+     * 	c方法出现异常: A回滚，B成功，C回滚，无法执行，外无法执行
+     * 场景3:
+     * 	外成了后，int i = 10/0; B, D成功。A,C,外都执行了但是必须回滚
+     * 场景4:
+     * 	D炸;抛异常。外事务感知到异常。A, c回滚，外执行不到，D自己回滚，成功
+     * 场景5:
+     * 	c用try-catch执行:出了异常回滚，由于异常被捕获，外事务投有感知异常。A,B,D都成功，C自己回滚
+     *
+     * 总结:
+     * 	--事务传播行为过程中，只要Requires_ new被执行过就一定成功。不管后面出不出问题。异常机制还是一样的，出现异常代码以后不执行。
+     * 	--Required只要感觉到异常就一定回滚。 和外事务是什么传播行为无关
+     * 	--传播行为总是来定义，当-个事务存在的时候，他内部的事务该怎么执行。
+     * 	#########################################
+     *
+     *
+     *
+     * @Transactional  一定不要标准在Controller
+     * //AOP做的事务
+     * //基于反射调用了
+     *  对象.方法（）才可以调用事务
+     *  本类调用自己的方法，相当于用的同一个事务saveProduct
+     *
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void saveProduct(PmsProductParam productParam) {
-        //1)、pms_product: 保存商品基本信息
+        //1)、pms_product: 保存商品基本信息-->>【REQUIRED】--此方法需要事务，如果没有就开新事务，如果之前已存在就用以前事务
         saveBaseInfo(productParam);
 
-        //2)、pms_product_attribute_value: 保存这个商品对应的所有属性的值
+        //5)、pms_sku_stock_sku_ 库存表-->>【REQUIRED】--此方法需要事务，如果没有就开新事务，如果之前已存在就用以前事务
+        saveSkuStock(productParam);
+
+        /*以下都可以try- cotch互不影响*/
+        //2)、pms_product_attribute_value: 保存这个商品对应的所有属性的值-->>REQUIRED_NEW 新开事务，各自互不影响
         saveProductAttributeValue(productParam);
 
-        //3)、pms_product_full_reduction: 保存商品的满诚信息
+        //3)、pms_product_full_reduction: 保存商品的满诚信息-->>REQUIRED_NEW 新开事务，各自互不影响
         saveProductFullReduction(productParam);
 
-        //4)、pms_product_ladder: 满减表
+        //4)、pms_product_ladder: 满减表-->>REQUIRED_NEW 新开事务，各自互不影响
         saveProductLadder(productParam);
 
-        //5)、pms_sku_stock_sku_ 库存表
-        saveSkuStock(productParam);
+
 
         /*
         * 使用ThreadLocal时遵守以下两个小原则 防止内存溢出:
@@ -112,6 +177,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
         threadLocal.remove();
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveSkuStock(PmsProductParam productParam) {
         productParam.getSkuStockList().forEach(skuStock -> {
             skuStock.setProductId(threadLocal.get());
@@ -125,6 +191,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
                 ,Thread.currentThread().getId(),Thread.currentThread().getName(),threadLocal.get(),map.get(Thread.currentThread()));
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveProductLadder(PmsProductParam productParam) {
         productParam.getProductLadderList().forEach(productLadder -> {
             productLadder.setProductId(threadLocal.get());
@@ -132,8 +199,10 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
         });
         log.debug("saveSkuStock-->>当前线程--{}-->线程名：{}--->productID:{}-->{}"
                 ,Thread.currentThread().getId(),Thread.currentThread().getName(),threadLocal.get(),map.get(Thread.currentThread()));
+        int i =1/0;
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveProductFullReduction(PmsProductParam productParam) {
         productParam.getProductFullReductionList().forEach(productFullReduction -> {
             productFullReduction.setProductId(threadLocal.get());
@@ -143,6 +212,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
                 ,Thread.currentThread().getId(),Thread.currentThread().getName(),threadLocal.get(),map.get(Thread.currentThread()));
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveProductAttributeValue(PmsProductParam productParam) {
         productParam.getProductAttributeValueList().forEach(productAttributeValue -> {
             //在pms_product_attribute_value表内需要设置其关联的productId
@@ -153,6 +223,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
                 ,Thread.currentThread().getId(),Thread.currentThread().getName(),threadLocal.get(),map.get(Thread.currentThread()));
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveBaseInfo(PmsProductParam productParam) {
         Product product = new Product();
         BeanUtils.copyProperties(productParam,product);//将前台传递过来的数据源内的相同属性copy过来赋值给product
